a20a3e67b8521a6a101b70c77e530ae3cfe3e5fc
Vittor Guilherme Borges de Oliveira <vittoroliveira.dev@gmail.com>
2024-09-23 21:33:12 -0300

    Add files via upload

---
+++ Aula40.py
@@ -0,0 +1,47 @@
+"""
+Calculadora com while
+"""
+
+def input_int_or_float(prompt):
+    while True:
+        user_input = input(prompt)
+        try:
+            # tenta converter para inteiro
+            return int(user_input)
+        except ValueError:
+            try:
+                # se falhar, tenta converter para float
+                return float(user_input)
+            except ValueError:
+                # se nenhum dos dois funcionar, pede novamente
+                print('Por favor, insira um número válido')
+
+def input_operador():
+    while True:
+        operador = input('Digite o operador (+, -, *, /): ')
+        if operador in ['+', '-', '*', '/']:  # Corrigido o erro de sintaxe
+            return operador
+        else:
+            print('Operador inválido. Por favor, escolha entre +, -, * ou /')
+
+# Exemplo de uso
+
+primeiro_numero = input_int_or_float('Digite o primeiro número: ')
+segundo_numero = input_int_or_float('Digite o segundo número: ')
+operador = input_operador()  # Corrigido o erro na chamada da função
+
+# Aplicando o operador lógico
+if operador == '+':
+    resultado = primeiro_numero + segundo_numero
+elif operador == '-':
+    resultado = primeiro_numero - segundo_numero
+elif operador == '*':
+    resultado = primeiro_numero * segundo_numero
+elif operador == '/':
+    if segundo_numero != 0:
+        resultado = primeiro_numero / segundo_numero
+    else:
+        resultado = "Erro: Divisão por zero!"
+
+print(f'O resultado de {primeiro_numero} {operador} {segundo_numero} é: {resultado}')
+
