1890631bb70247c4f1d0b966ea013c52b12514e3
Vittor Guilherme Borges de Oliveira <vittoroliveira.dev@gmail.com>
2024-09-23 21:34:04 -0300

    Add files via upload

---
+++ Aula40a.py
@@ -0,0 +1,51 @@
+"""
+Calculadora com while
+"""
+
+while True:
+    numero_1 = input('Digite um número: ')
+    numero_2 = input('Digite outro número: ')
+    operador = input('Digite um operador: (+ - / *)')
+
+ #   numeros_validos = None
+ #   num_1_float_int = 0
+ #   num_2_float_int = 0
+    
+    try:
+        num_1_float = float(numero_1)
+        num_2_float = float(numero_2)
+        numeros_validos = True
+        
+    except ValueError as error:
+        numeros_validos = False
+        print('Um ou ambos os números digitados são inválidos.')
+        continue
+
+    operadores_permitidos = '+ - / *'
+
+    if operador not in operadores_permitidos:
+        print('Operador inválido.')
+        continue
+
+    if len(operador) > 1:
+        print('Digite apenas um operador.')
+        continue
+
+    if operador == '+':
+        print(f'O resultado entre {num_1_float} + {num_2_float} é:', num_1_float + num_2_float)
+    elif operador == '-':
+        print(f'O resultado entre {num_1_float} - {num_2_float} é:', num_1_float - num_2_float)
+    elif operador == '/':
+        if num_2_float == 0:
+            print('Não é possível dividir por zero.')
+        else:
+            print(f'O resultado entre {num_1_float} / {num_2_float} é:', num_1_float / num_2_float)
+    elif operador == '*':
+        print(f'O resultado entre {num_1_float} * {num_2_float} é:', num_1_float * num_2_float)
+    else:
+        print('Não foi possível calcular.')
+
+    sair = input('Quer sair? [s]im: ').lower().startswith('s')
+   
+    if sair is True:
+        break
